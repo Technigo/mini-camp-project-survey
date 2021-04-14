b38f3786c525ce964a07d159b3559df96ace51c6
Jani <jani@muikku.org>
2021-04-14 20:23:05 +0200

    some design updates

--- code/src/App.js
+++ code/src/App.js
@@ -7,7 +7,6 @@ import Button from './components/Button';
 import './index.css';
 
 export const App = () => {
-  /* eslint-disable no-unused-vars */
   const [replyExperience, updateExperience] = useState([]);
   const [replyWeeks, updateWeeks] = useState([]);
   const [replyFavourite, updateFavourite] = useState([]);
@@ -16,7 +15,6 @@ export const App = () => {
   const [currentQuestion, updateCurrentQuestion] = useState(1);
   const questionData = data.questions.filter((item) => item.question_id === currentQuestion);
   const totalQuestions = data.questions.length;
-  /* eslint-enable no-unused-vars */
   function storeReply(questionID, reply) {
     if (questionID === 1) {
       updateExperience(reply)
@@ -44,9 +42,13 @@ export const App = () => {
       {surveyOver ? (
         <div id={`card-${totalQuestions}`} className="card card-summary inactive">
           <h1>Summary</h1>
+          <p>{data.questions[0].question_text}</p>
           <p>{replyExperience || null}</p>
+          <p>{data.questions[1].question_text}</p>
           <p>{replyWeeks || null}</p>
+          <p>{data.questions[2].question_text}</p>
           <p>{replyFavourite || null}</p>
+          <p>{data.questions[3].question_text}</p>
           {replyImprove.checkboxValues ? (
             Object.keys(replyImprove.checkboxValues).map((item, i) => (
               <span className="checkbox-items" key={i}>{item}</span>
@@ -79,6 +81,10 @@ export const App = () => {
           })}
         </form>
       )}
+      <div className="circle circle-1" />
+      <div className="circle circle-2" />
+      <div className="circle circle-3" />
+      <div className="circle circle-4" />
     </div>
   );
 }
